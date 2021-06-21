04ad35d861c75892631827e991045823a9c3c6d7
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-21 14:29:26 +0200

    closing project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 //TODO Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 //TODO Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-
+const app = new Vue({
+    el : '#app',
+    data : {
+        messaggio : 'Titolone',
+        image : 'img/img1.jpg'
+    }
+});
